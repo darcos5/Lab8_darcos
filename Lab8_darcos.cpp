15abf440fd09ad746743663d675ec6f5ec092398
darcos5 <darcos@student.cscc.edu>
2026-03-29 16:22:27 -0400

    created file with basic comments/code

---
+++ Lab8_darcos.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file Lab8_darcos.cpp
+ * @author Daisy Arcos
+ * @date 2026-03-29
+ * @brief Demonstrates stack vs heap memory and the use of pointers and references.
+ */
+
+ //creat a program that manipulates interger varaibles in different memory locations 
+ //and access them through pointers and refrences 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// add your code here
+
+	return 0;
+}
